f5d7c03282630ffc10a152dde5582681830ee272
Isa Herico <miss.herico@gmail.com>
2018-09-04 08:26:32 -0700

    App now able to use node to serve files

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-var express = require('express');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const webpack = require('webpack');
+const middleware = require('webpack-dev-middleware');
+
+const wbOptions = require('./webpack.config.js');
+const compiler = webpack({ ...wbOptions });
+
+app.use(middleware(compiler));
 
 app.get('/favicon.ico', (req, res) => {
   res.sendFile(path.join(__dirname, './dist/favicon.ico'))
@@ -7,3 +17,5 @@ app.get('/favicon.ico', (req, res) => {
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './dist/index.html'))
 });
+
+app.listen(3000);
